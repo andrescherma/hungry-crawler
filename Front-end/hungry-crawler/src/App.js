836916873ad762b421458f2a26171b872779fde3
André Scherma <>
2018-06-07 17:58:30 -0300

    trying to make LinksList component react to prop changes

--- Front-end/hungry-crawler/src/App.js
+++ Front-end/hungry-crawler/src/App.js
@@ -5,6 +5,43 @@ import './css/App.css';
 
 class App extends Component {
 
+  constructor () {
+    super();
+    this.state = {
+      search: '',
+      searchKey: '',
+    };
+
+    this.searchFromUrl = this.searchFromUrl.bind(this);
+    this.handleUrlInput = this.handleUrlInput.bind(this);
+    this.handleCrawlResponse = this.handleCrawlResponse.bind(this);
+  }
+
+
+  searchFromUrl (e) {
+    e.preventDefault();
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = this.handleCrawlResponse;
+    xhr.open("POST", 'http://localhost:8080/crawl', true);
+    xhr.send(JSON.stringify({
+        url: this.state.search,
+    }));
+  }
+
+
+  handleUrlInput (e) {
+    e.preventDefault();
+    this.setState({search: e.target.value});
+  }
+
+
+  handleCrawlResponse (e) {
+    this.setState({searchKey: e.target.responseText});
+  }
+
+
   render() {
     return (
       <div className="App container-fluid">
@@ -19,15 +56,18 @@ class App extends Component {
 
           <div className="input-box">
 
-            <form className="col-sm-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
-              <input type="text" />
+            <form 
+              onSubmit={this.searchFromUrl}
+              className="col-sm-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3"
+            >
+              <input type="text" value={this.state.search} onChange={this.handleUrlInput} />
               <input type="submit" value="Crawl!"/>
             </form>
 
           </div>
         </div>
 
-        <LinksList />
+        <LinksList searchKey={this.state.searchKey} />
         
       </div>
     );
--- Front-end/hungry-crawler/src/LinksList.js
+++ Front-end/hungry-crawler/src/LinksList.js
@@ -1,33 +1,49 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './css/App.css';
 import LinksListItem from './LinksListItem';
 
 import firebase from './firebase.js';
+
 class LinksList extends Component {
 
+  
   constructor(props) {
     super(props);
-    this.state = {links: {}};
-  };
 
-  /*
-  componentDidMount () {
-    firebase.database().ref('urls').on('value', function(snap){
-      var links = snap.val();
-      console.log(links);
-      this.setState({
-        links: links
-      });
-    });
+    this.state = {};
+    this.updateLinks.bind(this);
   }
-  */
+  
+  
+  componentWillMount () {
+    var searchKey = this.props.searchKey;
+    if (searchKey !== '') {
+      firebase.database().ref('urls/' + searchKey).on('value',
+        snap => this.updateLinks(snap.val())
+      );
+    }
+  }
 
+  componentDidUpdate () {
+    var searchKey = this.props.searchKey;
+    if (searchKey !== '') {
+      firebase.database().ref('urls/' + searchKey).on('value',
+        snap => this.updateLinks(snap.val())
+      );
+    }
+  }
+
+
+  updateLinks(linksDict) {
+    this.setState({links: linksDict});
+  }
+  
   render () {
+    var links = this.state.links || {};
+
     return(
       <div className="urls-list list-row">
         <ul className="list-group col-sm-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
-          <LinksListItem link='http://teamtreehouse.com' />
+          {Object.keys(links).map(key => <LinksListItem link={links[key]} />)}
         </ul>
       </div>
   )};
